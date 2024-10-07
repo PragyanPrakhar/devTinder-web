d2a507a959baee93f7087d5b5bad0e8c3c7a78cc
Pragyan Prakhar <pragyanprakhar@gmail.com>
2024-10-07 10:19:43 +0530

    Made the routes protected

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,13 +1,39 @@
-import { Outlet } from "react-router";
+import { Outlet, useNavigate } from "react-router";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import { BASE_URL } from "../utils/constants";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
 
 const Body = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const userData = useSelector((store) => store.user);
+
+    const fetchUser = async () => {
+        if(userData) return ;
+        try {
+            const user = await axios.get(BASE_URL + "/profile/view", {
+                withCredentials: true,
+            });
+            dispatch(addUser(user.data));
+        } catch (error) {
+            if (error.status === 401) {
+                navigate("/login");
+            }
+            console.error(error);
+        }
+    };
+    useEffect(() => {
+        fetchUser();
+    }, []);
     return (
         <div>
             <Navbar />
             <Outlet />
-            <Footer/>
+            <Footer />
         </div>
     );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,17 @@
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     const user = useSelector((store) => store.user);
-    console.log("UseSelector", user);
+    // console.log("UseSelector", user);
     return (
         <div className="navbar bg-base-300">
             <div className="flex-1">
-                <a className="btn btn-ghost text-xl">
+                <Link to="/" className="btn btn-ghost text-xl">
                     <div className="text-white font-bold text-3xl">
                         <span className="text-orange-500">dev</span>Tinder
                     </div>
-                </a>
+                </Link>
             </div>
             {user && (
                 <div className="flex-none gap-2">
@@ -30,10 +31,10 @@ const Navbar = () => {
                             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
                         >
                             <li>
-                                <a className="justify-between">
+                                <Link to="/profile" className="justify-between">
                                     Profile
                                     <span className="badge">New</span>
-                                </a>
+                                </Link>
                             </li>
                             <li>
                                 <a>Settings</a>

e7f3239dc45301519f5707d387c6a09935f748c5
Pragyan Prakhar <pragyanprakhar@gmail.com>
2025-01-16 23:43:29 +0530

    Changed the UI after the payment verification

--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -1,8 +1,17 @@
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import { BASE_URL } from "../utils/constants";
 
 const Premium = () => {
+    const [isUserPremium, setIsUserPremium] = useState(false);
+    const verifyPremiumUsers = async () => {
+        const result = await axios.get(BASE_URL + "/premium/verify", {
+            withCredentials: true,
+        });
+        if (result.data.isPremium) {
+            setIsUserPremium(true);
+        }
+    };
     const handleBuyClick = async (type) => {
         const order = await axios.post(
             BASE_URL + "/payment/create",
@@ -15,7 +24,7 @@ const Premium = () => {
         );
         //After getting the positive response , the razorpay dialog box should be opeaned.
         // This Razorpay will come from the script tag which has been added in the head tag of the index.html.
-        const {amount,currency,keyId,notes,orderId}=order.data;
+        const { amount, currency, keyId, notes, orderId } = order.data;
         const options = {
             //It is the same key id of the razorpay account , And It can be public so, we can keep it in the frontend code.
             key: keyId, // Replace with your Razorpay key_id
@@ -25,20 +34,23 @@ const Premium = () => {
             description: "Connect to the other developers",
             order_id: orderId, // This is the order_id created in the backend
             prefill: {
-                name:notes.firstName+" "+notes.lastName,
+                name: notes.firstName + " " + notes.lastName,
                 email: notes.emailId,
                 contact: "9999999999",
             },
             theme: {
                 color: "#F37254",
             },
+            handler: verifyPremiumUsers,
         };
 
         const rzp = new window.Razorpay(options);
         rzp.open();
     };
 
-    return (
+    return isUserPremium ? (
+        "You are already a premium User."
+    ) : (
         <div className="m-10">
             <div className="flex w-full">
                 <div className="card bg-base-300 rounded-box grid h-80 flex-grow place-items-center">
